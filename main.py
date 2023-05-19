5653741bc2af256d058ed76922ace7f1f62833a4
BWsix <radioactive-lollipop@proton.me>
2023-05-19 22:57:47 +0800

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+import interactions
+from dotenv import dotenv_values
+
+bot = interactions.Client()
+
+config = dotenv_values(".env")
+assert "TOKEN" in config
+
+
+@interactions.slash_command(name="upload", description="Upload Anonymously")
+async def my_command_function(ctx: interactions.SlashContext):
+    form = interactions.Modal(
+        interactions.ParagraphText(
+            label="Description",
+            placeholder="Enter a description",
+            custom_id="description",
+            required=True,
+        ),
+        interactions.ShortText(
+            label="Image URL",
+            placeholder="Enter an image URL",
+            custom_id="image_url",
+            required=False,
+        ),
+        title="Upload Anonymously",
+    )
+    await ctx.send_modal(modal=form)
+
+    form_ctx: interactions.ModalContext = await ctx.bot.wait_for_modal(form)
+    res = form_ctx.responses
+    await form_ctx.channel.send(
+        embed=interactions.Embed(
+            description=res["description"],
+            images=[interactions.EmbedAttachment(url=res["image_url"])],
+        )
+    )
+    await form_ctx.send("Uploaded!", ephemeral=True)
+
+
+bot.start(config["TOKEN"])
