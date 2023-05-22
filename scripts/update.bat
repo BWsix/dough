10a03b8f85a10b083b71df673a4ee9a141f713f5
BWsix <radioactive-lollipop@proton.me>
2023-05-22 22:34:45 +0800

    feat: improve update process

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,1 +1,1 @@
-cd .. && git pull && pip install -r requirements.txt
+cd .. && git pull && pip install -r requirements.txt && cls && echo After configuring the bot, run scripts\restart.bat to restart the bot. && pause
